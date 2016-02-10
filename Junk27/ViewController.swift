6e11feff4c2f23e4ac32b169da946662749b92e0
See.Ku <info@see-ku.com>
2016-02-10 16:07:31 +0900

    Initial Commit

---
+++ Junk27/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Junk27
+//
+//  Created by See.Ku on 2016/02/10.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
